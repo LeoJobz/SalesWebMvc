b7a2d78b68eaba015178f1bf87e47ca515d994fd
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-06-03 15:43:52 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,11 +1,8 @@
-﻿using SalesWebMvc.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services.Exceptions;
-using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
